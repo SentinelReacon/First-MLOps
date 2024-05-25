41d0b6c764c87a907c801bc4755fc5f208d55302
Amogh Joshi <amoghjoshi500@gmail.com>
2024-05-25 14:54:37 +0530

    Update the changes

---
+++ src/model_dev.py
@@ -0,0 +1,30 @@
+import logging
+from abc import ABC, abstractmethod
+from sklearn.linear_model import LinearRegression
+
+class Model(ABC):
+    
+    """
+    this is going to be the abstract class for all the models
+    """
+    
+    @abstractmethod
+    def train(xtrain, ytrain):
+        
+        pass
+    
+
+class LinearRegressionModel(Model):
+    
+    def train(xtrain, ytrain):
+        
+        try:
+            lr = LinearRegression()
+            lr.fit(xtrain, ytrain)
+            logging.info("Model training completed")
+            
+            return lr
+        except Exception as e:
+            logging.error(f"Error in training the model {e}")
+            raise e
+        
--- steps/train_model.py
+++ steps/train_model.py
@@ -1,9 +1,30 @@
 import logging
 import pandas as pd
 from zenml import step
+from src.model_dev import LinearRegressionModel
+from sklearn.base import RegressorMixin
+from.config import ModelNameConfig
 
 @step
 def training(
-    df: pd.DataFrame
-) -> None:
-    pass
+    xtrain: pd.DataFrame,
+    xtest: pd.DataFrame,
+    ytrain: pd.Series,
+    ytest: pd.Series, 
+    config: ModelNameConfig
+) -> RegressorMixin:
+    
+    
+    model = None
+    
+    try:
+        if config.model_name == "Linear Regression":
+            model - LinearRegressionModel()
+            trained_model = model.train(xtrain, ytrain)
+            return trained_model
+        
+        else: 
+            raise ValueError("Model not supported")
+            
+    except Exception as e:
+        logging.error(f"Model training not supported {e}")
